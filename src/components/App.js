57dfabdf79484d4d00ebea353d13415942709128
Utkarsh Yadav <utkarshyadav_as@srmuniv.edu.in>
2020-09-06 14:09:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -31,7 +31,7 @@ class App extends Component{
     var hashParams = {};
     var e, r = /([^&;=]+)=?([^&;]*)/g,
         q = window.location.hash.substring(1);
-    while ( e !== r.exec(q)) {
+    while ( e === r.exec(q)) {
        hashParams[e[1]] = decodeURIComponent(e[2]);
     }
     return hashParams;
